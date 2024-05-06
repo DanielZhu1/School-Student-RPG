63cc1a197cec7e18cc95ad631d92665777025f46
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-06 08:41:37 -0400

    Update main.py 5/6/2024

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import pygame
+
+from mc import MC
+
+pygame.init()
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 700
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode((size))
+r= 50
+g= 0
+b= 100
+game_over = False
+if MC.hp  == 0:
+    game_over = True
+
 print("Welcome to School Student RPG")
+run = True
+
+while run:
+    screen.fill((r, g, b))
+    pygame.display.update()
+pygame.quit()
