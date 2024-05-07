6641025264a6c16c274f9e64da33025623e878de
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-07 08:42:20 -0400

    Update mc.py 05/07/2024

--- mc.py
+++ mc.py
@@ -1,29 +1,42 @@
 import pygame
 
 import random
+
+from nerd_enemy import Enemy
 class MC:
-    def __init__(self, hp, bp, xp):
+    def __init__(self, hp, bp):
         self.hp = hp
         self.bp = bp
-        self.xp = xp
         hp = 120
         bp = 50
-        xp = 0
+
+    def turn(self):
+        if Using_Skills = True or Guard = True:
 
     def skills(self, hp, bp):
         self.hp = hp
         self.bp = bp
         blunt_hit = random.randint(50, 70)
-        blunt_hit.cost = 10 hp
+        blunt_hit.cost = hp - 10
         lock_in = damage_dealt * 2.5
-        lock_in.cost = 20 bp
+        lock_in.cost = bp - 20
         yap_session = random.randint(0, 100)
-        if yap_session > 75:
+        if yap_session > 85:
             enemy.defeated
-        yap_session.cost = 8 bp
-    def Using_Skills(self, hp, bp):
+        yap_session.cost = bp - 6
+        math_attack = random.randint(45, 65)
+        math_attack.cost = bp - 4
+    def Guard(self, hp):
+        self.hp = hp
+        Enemy.damage_received * 0.2
+    def Using_Skills(self, hp, bp, blunt_hit, lock_in, yap_session, math_attack):
         self.hp = hp
         self.bp = bp
-        current_hp = self.hp - damage_taken
-        current_bp = self.bp - skill_cost
-        
+        if hp < str(blunt_hit.cost):
+            print("You do not have enough HP!")
+        if bp < str(lock_in.cost):
+            print("You do not have enough BP!")
+        if bp < str(yap_session.cost):
+            print("You do not have enough BP!")
+        if bp < str(math_attack.cost):
+            print("You do not have enough BP!")
