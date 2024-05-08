1c27fee280e1ada6cb53fe5af4e471643981eece
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-08 08:43:19 -0400

    Update mc.py 05/08/2024

--- mc.py
+++ mc.py
@@ -3,36 +3,36 @@ import pygame
 import random
 
 from nerd_enemy import Enemy
-class MC:
+class Main_character:
+    hp = 120
+    bp = 50
+
     def __init__(self, hp, bp):
         self.hp = hp
         self.bp = bp
-        hp = 120
-        bp = 50
-
-    def turn(self):
-        if Using_Skills = True or Guard = True:
 
     def skills(self, hp, bp):
         self.hp = hp
         self.bp = bp
         blunt_hit = random.randint(50, 70)
         blunt_hit.cost = hp - 10
-        lock_in = damage_dealt * 2.5
+        lock_in = 50 * 2.5
         lock_in.cost = bp - 20
         yap_session = random.randint(0, 100)
         if yap_session > 85:
-            enemy.defeated
+            Enemy.hp = 0
         yap_session.cost = bp - 6
         math_attack = random.randint(45, 65)
         math_attack.cost = bp - 4
+
     def Guard(self, hp):
         self.hp = hp
         Enemy.damage_received * 0.2
+
     def Using_Skills(self, hp, bp, blunt_hit, lock_in, yap_session, math_attack):
         self.hp = hp
         self.bp = bp
-        if hp < str(blunt_hit.cost):
+        if hp <= str(blunt_hit.cost):
             print("You do not have enough HP!")
         if bp < str(lock_in.cost):
             print("You do not have enough BP!")
