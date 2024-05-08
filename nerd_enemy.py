ebd05034b38cc12dc0e371610b6aaa9e65298b0c
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-08 08:43:48 -0400

    Update nerd_enemy.py 05/08/2024

--- nerd_enemy.py
+++ nerd_enemy.py
@@ -2,15 +2,13 @@ import pygame
 
 import random
 
-from mc import MC
-
-
 class Enemy:
+    hp = 320
+
     def __init__(self, hp):
         self.hp = hp
-        hp = 320
 
-    def attacks(self):
+
+    def attacks(self, Main_character):
         Erm_actually = random.randint(20, 25)
-        damage_received = MC.hp - int(Erm_actually)
-
+        damage_received = Main_character.hp - int(Erm_actually)
