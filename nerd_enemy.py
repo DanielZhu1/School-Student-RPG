a344d5ae060c09e16329fed2466d6e99812c73db
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-10 08:44:52 -0400

    Update nerd_enemy.py 05/10/2024

--- nerd_enemy.py
+++ nerd_enemy.py
@@ -2,13 +2,19 @@ import pygame
 
 import random
 
+
 class Enemy:
     hp = 320
 
     def __init__(self, hp):
         self.hp = hp
 
-
     def attacks(self, Main_character):
         Erm_actually = random.randint(20, 25)
         damage_received = Main_character.hp - int(Erm_actually)
+        lock_in = Erm_actually * 2.5
+    if Enemy.hp < 80:
+        lock_in.use
+    if Enemy.hp <= 0:
+        game_over = True
+
