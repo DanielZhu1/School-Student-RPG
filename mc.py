c5e6d512020c07eaedd575b8a0305d5d9058c50f
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-20 08:39:48 -0400

    Update mc.py 05/20/2024

--- mc.py
+++ mc.py
@@ -5,37 +5,81 @@ import random
 from nerd_enemy import Enemy
 
 
-class Main_character:
+class Main_Character:
     max_hp = 120
     max_bp = 50
-    action_cooldown = 0
+    action_count = 1
     action_wait_time = 90
     hp = max_hp - Enemy.damage_received
 
-    def __init__(self, hp, bp, max_hp, max_bp, alive):
+    def __init__(self, hp, bp, max_hp, max_bp, alive, lock_in, blunt_hit, yap_session, math_attack, guard, Using_Skills, action_count, Casting_Skills):
         self.hp = hp
         self.bp = bp
         self.max_hp = max_hp
         self.max_bp = max_bp
         self.alive = alive
+        self.lock_in = lock_in
+        self.blunt_hit = blunt_hit
+        self.yap_session = yap_session
+        self.math_attack = math_attack
+        self.guard = guard
+        self.Using_Skills = Using_Skills
+        self.action_count = action_count
+        self.Casting_Skills = Casting_Skills
 
-    def skills(self, hp, bp, ):
+    def blunt_hit(self, hp, max_hp, Using_Skills, blunt_hit, action_count, Casting_Skills):
         self.hp = hp
-        self.bp = bp
-        blunt_hit = random.randint(50, 70)
-        blunt_hit.cost = hp - 10
-        yap_session = random.randint(0, 100)
-        if yap_session > 85:
-            Enemy.hp = 0
-        yap_session.cost = bp - 6
-        math_attack = random.randint(45, 65)
-        math_attack.cost = bp - 4
-        lock_in = math_attack * 2.5
-        lock_in.cost = bp - 20
+        self.max_hp = max_hp
+        self.Using_Skills = Using_Skills
+        self.blunt_hit = blunt_hit
+        self.Casting_Skills = Casting_Skills
+        blunt_hit = random.randint(45, 60)
+        if Casting_Skills(blunt_hit):
+            hp = hp - 10
+            Enemy.hp - blunt_hit
+            action_count -= 1
 
-    def Guard(self, hp):
+    def yap_session(self, bp, max_bp, Using_Skills, yap_session, action_count, Casting_Skills):
+        self.bp = bp
+        self.max_bp = max_bp
+        self.Using_Skills = Using_Skills
+        self.yap_session = yap_session
+        self.Casting_Skills = Casting_Skills
+        yap_session = random.randint(0, 100)
+        if Casting_Skills(yap_session):
+            bp = bp - 8
+            action_count -= 1
+            if yap_session <= 80:
+                Enemy.alive = False
+
+    def math_attack(self, bp, max_bp, Using_Skills, math_attack, action_count,Casting_Skills):
+        self.bp = bp
+        self.max_bp = max_bp
+        self.Using_Skills = Using_Skills
+        self.math_attack = math_attack
+        self.Casting_Skills = Casting_Skills
+        math_attack = random.randint(40, 55)
+        if Casting_Skills(math_attack):
+            bp = bp - 4
+            Enemy.hp - math_attack
+            action_count -= 1
+
+
+    def lock_in(self, bp, lock_in, math_attack, Using_Skills, action_count, Casting_Skills):
+        self.bp = bp
+        self.lock_in = lock_in
+        self.Casting_Skills = Casting_Skills
+        lock_in = math_attack * 2.5
+        if Casting_Skills(lock_in):
+            bp = bp - 20
+            math_attack * 2.5
+            action_count -= 1
+
+
+    def guard(self, hp, action_count):
         self.hp = hp
         Enemy.damage_received * 0.2
+        action_count -= 1
 
     def Using_Skills(self, hp, bp, blunt_hit, lock_in, yap_session, math_attack):
         self.hp = hp
@@ -48,26 +92,25 @@ class Main_character:
             print("You do not have enough BP!")
         if bp < str(math_attack.cost):
             print("You do not have enough BP!")
-    if hp > 0:
-        action_cooldown += 1
 
-    def Casting_Skills(self, hp, bp, blunt_hit, lock_in, yap_session, math_attack, action_cooldown, action_wait_time):
-        while action_cooldown >= action_wait_time:
-            if keys.[pygame.K_1]:
+    if hp > 0:
+        action_count += 1
+    def Casting_Skills(self, hp, bp, blunt_hit, lock_in, yap_session, math_attack, action_count, action_wait_time):
+        while action_count >= action_wait_time:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_1]:
                 Enemy.hp - blunt_hit
                 hp - 10
-            if keys.[pygame.K_2]:
-                self.lock_in
+            if keys[pygame.K_2]:
+                lock_in = True
                 bp - 20
-            if keys.[pygame.K_3]:
+            if keys[pygame.K_3]:
                 yap_session
                 bp - 8
             if keys[pygame.K_4]:
                 Enemy.hp - math_attack
                 bp - 4
-                    
-                
-            
+
     if hp <= 0:
         game_over = True
-        
+
