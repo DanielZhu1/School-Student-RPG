369f34c1e00afbe018209823ffe0f152a442fc42
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-05-20 08:40:31 -0400

    Update nerd_enemy.py 05/20/2024

--- nerd_enemy.py
+++ nerd_enemy.py
@@ -10,12 +10,13 @@ class Enemy:
 
     Enemy.hp = 320
 
-    def __init__(self, hp):
+    def __init__(self, hp, alive):
         self.hp = hp
-
-    def attacks(self, Main_character, Erm_actually, damage_received):
+        self.alive = alive
+        alive = True
+    def attacks(self, Main_Character, Erm_actually, damage_received):
         Erm_actually = random.randint(20, 25)
-        damage_received = Main_character.hp - int(Erm_actually)
+        damage_received = Main_Character.hp - int(Erm_actually)
         if Enemy.hp < 80:
             print("Time to lock in!")
             Erm_actually * 2.5
