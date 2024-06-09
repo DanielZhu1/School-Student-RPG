c3e0cba5229fcd0e92478e9e56c4eee00a13a2ec
DanielZhu1 <167535264+DanielZhu1@users.noreply.github.com>
2024-06-09 19:30:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame
-
 from protag import Main_Character
-
 from nerd_enemy import Enemy
+
 
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont("Lobster", 20)
+my_font = pygame.font.SysFont("Arial", 30)
 game_over_font = pygame.font.SysFont("Impact", 100)
 pygame.display.set_caption("School Student RPG")
 SCREEN_HEIGHT = 800
@@ -22,22 +21,53 @@ b = 100
 
 victory = "You have won the game!"
 failure = "You have lost the game."
-game_over = False
-if Main_Character.hp == 0:
-    game_over = True
-    game_over_font.render(failure, True, (255, 255, 255))
-if Enemy.hp == 0:
-    game_over = True
-    game_over_font.render(victory, True, (255, 255, 255))
-# Sets up the win and loss conditions
-
+mc_turn = "It is main char turn!"
+enemy_turn = "It is enemy turn!"
 intro = "Welcome to School Student RPG"
+game_over = False
+# Sets up the win and loss conditions
+enemy = Enemy(100, 10, 600, 200)
+protag = Main_Character(100, 10, 20, 200)
 display_intro = my_font.render(intro, True, (255, 255, 255))
+display_mc_turn = my_font.render(mc_turn, True, (255, 55, 255))
+display_enemy_turn = my_font.render(enemy_turn, True, (255, 255, 25))
+display_enemy_hp = my_font.render("ENEMY HP: " + str(enemy.hp), True, (255, 255, 25))
+display_mc_hp = my_font.render("MC HP: " + str(protag.hp), True, (255, 255, 25))
 run = True
+start = False
+my_turn = True
 
 while run:
-    screen.fill((r, g, b))
-    screen.blit(background, (0, 0))
+
+    if protag.hp == 0:
+        game_over = True
+        game_over_font.render(failure, True, (255, 255, 255))
+    if enemy.hp == 0:
+        game_over = True
+        game_over_font.render(victory, True, (255, 255, 255))
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            start = True
+
+    if not(start):
+        screen.blit(background, (0, 0))
+    else:
+        screen.fill((r, g, b))
+        screen.blit(enemy.image, enemy.rect)
+        screen.blit(protag.image, protag.rect)
+        screen.blit(display_mc_hp, (120, 450))
+        screen.blit(display_enemy_hp, (500, 450))
+        if my_turn:
+            screen.blit(display_mc_turn, (300, 600))
+        elif enemy_turn:
+            screen.blit(display_enemy_turn, (300, 600))
+
+        # screen.fill((r, g, b))
+
     pygame.display.update()
+
 pygame.quit()
 # Ends the program
--- protag.py
+++ protag.py
@@ -1,11 +1,11 @@
 import pygame
 
-class Enemy:
+class Main_Character:
     def __init__(self, hp, attack, x, y):
         self.hp = hp
         self.x = x
         self.y = y
-        self.image = pygame.image.load("nerd_enemy.png")
+        self.image = pygame.image.load("protagonist.png")
         self.attack = attack
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
@@ -14,7 +14,3 @@ class Enemy:
         self.image = pygame.transform.scale(self.image, scale_size)
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-
-
-# #
